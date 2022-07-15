1a82c66fc0c67aeb104d4eb61a56ad6feb072411
Siyuan Zuo <53063815+legenddaniel@users.noreply.github.com>
2022-07-15 18:21:12 -0400

    Create hbase.sh

---
+++ hbase.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# wget https://cmake.org/files/v3.10/cmake-3.10.0.tar.gz
+# tar xzf cmake-3.10.0.tar.gz
+# cd cmake-3.10.0
+# ./bootstrap --prefix=/usr
+# make
+# sudo make install
+# sudo yum -y install git
+# echo "Pre Pip"
+# python --version
+# sudo /usr/bin/pip3 install h3
+# sudo /usr/bin/pip3 install boto3
+# sudo /usr/bin/pip3 install analytics-python
+# echo "Post pip"
+# sudo aws s3 cp s3://gli-init-scripts/delta-core_2.11-0.5.0.jar /usr/lib/spark/jars/
+# sudo aws s3 cp s3://gli-init-scripts/postgresql-42.2.12.jar /usr/lib/spark/jars/
+
+# Install Golang
+apt-get update
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
+go version
+echo "$GOPATH"
+# Clone repos. We need all those server side repos. Now use the modified branch. (Ignore this now)
+
+# The data processing should be in `step` instead of here because HBase is installed after bootstraping
